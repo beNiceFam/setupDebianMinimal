136ef250ceb22f028ae68a75d26c4067aeccb4ea
beNiceFam <133543297+beNiceFam@users.noreply.github.com>
2023-08-30 14:22:36 -0300

    Update setup.sh
    Setting up Flatpak and the i3wm config file

--- setup.sh
+++ setup.sh
@@ -5,10 +5,10 @@ echo 'APT::Install-Recommends "0" ; APT::Install-Suggests "0" ;' >> /etc/apt/apt
 apt update
 apt install network-manager-gnome 
 echo -e "source /etc/network/interfaces.d/*\nauto lo\niface lo inet loopback" > /etc/network/interfaces
-apt install neovim lightdm feh fonts-jetbrains-mono alacritty htop gvfs polybar i3-wm rofi zathura xserver-xorg-video-all 
-apt purge nano && sudo apt autoremove 
+apt install neovim lightdm feh xfce4-power-manager flatpak fonts-jetbrains-mono alacritty htop gvfs polybar i3-wm rofi zathura xserver-xorg-video-all 
+apt purge nano && apt autoremove 
 exit
+rm ~/.i3/config ~/config/i3/config && cp $PWD/i3config ~/.i3/config
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 systemctl reboot
-
-
 
