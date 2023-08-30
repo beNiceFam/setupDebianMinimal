15fd60ae7dea5be4af4c3c8857220d9980c2b6b4
beNiceFam <133543297+beNiceFam@users.noreply.github.com>
2023-08-30 14:48:18 -0300

    Update setup.sh
    Set up flatpak. Now the script executes properly using the Bash shell and sudo. Previously it would not since it was the root user

--- setup.sh
+++ setup.sh
@@ -1,13 +1,8 @@
-#!/bin/bash
-
-sudo -i
-echo 'APT::Install-Recommends "0" ; APT::Install-Suggests "0" ;' >> /etc/apt/apt.conf
-apt update
-apt install network-manager-gnome 
-echo -e "source /etc/network/interfaces.d/*\nauto lo\niface lo inet loopback" > /etc/network/interfaces
-apt install neovim lightdm feh xfce4-power-manager flatpak fonts-jetbrains-mono alacritty htop gvfs polybar i3-wm rofi zathura xserver-xorg-video-all 
-apt purge nano && apt autoremove 
-exit
+sudo bash -c 'echo "APT::Install-Recommends "0" ; APT::Install-Suggests "0" ;" >> /etc/apt/apt.conf; apt update;
+apt install network-manager-gnome; 
+echo -e "source /etc/network/interfaces.d/*\nauto lo\niface lo inet loopback" > /etc/network/interfaces;
+apt install neovim lightdm feh xfce4-power-manager flatpak fonts-jetbrains-mono alacritty htop gvfs polybar i3-wm rofi zathura xserver-xorg-video-all; 
+apt purge nano && apt autoremove'
 rm ~/.i3/config ~/config/i3/config && cp $PWD/i3config ~/.i3/config
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 systemctl reboot
